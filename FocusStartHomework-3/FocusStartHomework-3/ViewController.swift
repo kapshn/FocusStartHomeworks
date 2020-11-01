73db9b28dc5cf384589f1ee64c4b6bc1de044720
Igor Pervushin <virus_220v@mail.ru>
2020-11-01 13:54:19 +0700

    initial project commit

---
+++ FocusStartHomework-3/FocusStartHomework-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FocusStartHomework-3
+//
+//  Created by Monday MeoW. on 01.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
